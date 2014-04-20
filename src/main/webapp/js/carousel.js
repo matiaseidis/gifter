30b89db4417a0b34acbf99c0483bcadde3d006ef
matiaseidis <matiaseidis@gmail.com>
2014-04-20 18:27:15 -0300

    carousel...

--- src/main/webapp/js/carousel.js
+++ src/main/webapp/js/carousel.js
@@ -3,7 +3,7 @@ $(function(){
 	$.carouselItem = function(from, to, images, modalSelector) {
 		var item = $("<div />", {class:"item"});
 		var itemInnerLeft = $("<div />", {class:"col-md-1"});
-		var itemInnerCenter = $("<div />", {class:"col-md-10"});
+		var itemInnerCenter = $("<div />", {class:"item-elements col-md-10"});
 		var itemInnerRight = $("<div />", {class:"col-md-1"});
 		
 		if(from == 0) {
@@ -11,6 +11,7 @@ $(function(){
 		}
 		for(var i = from; i<to; i++) {
 			var img = $("<img />", {src:images[i]});
+			img.css("border", "1px solid #ddd");
 			if(i == 0) {
 				img.addClass("first");
 				$(modalSelector + " .modal-body-down img").attr("src", images[i]);
@@ -32,20 +33,28 @@ $(function(){
 			id: "carousel-"+item.id });
 		var body = $("<div />", {class:"carousel-inner"});
 
-		var carouselItemSize = 4;
+		var carouselItemSize = 5;
+		var availableImages = item.items[0].images.length; 
+		var needCarousel = availableImages > carouselItemSize; 
 		
-		if(item.items[0].images.length <= carouselItemSize) {
-			console.log("A apending form " + 0 + " to " + item.items[0].images.length + " of " + item.items[0].images.length);
-			body.append($.carouselItem(0, item.items[0].images.length, item.items[0].images, modalSelector));
-		} else {
-			var rest = Math.floor(item.items[0].images.length % carouselItemSize);
-			var completeCarouselItems = Math.floor(item.items[0].images.length / carouselItemSize);
+//		if(!needCarousel) {
+//			
+//			console.log("A apending form " + 0 + " to " + availableImages + " of " + availableImages);
+//			
+//			body.append($.carouselItem(0, availableImages, item.items[0].images, modalSelector));
+//			
+//			root.append(body);
+//			
+//			
+//		} else {
+			var rest = Math.floor(availableImages % carouselItemSize);
+			var completeCarouselItems = Math.floor(availableImages / carouselItemSize);
 			
 			console.log("completeCarouselItems: " + completeCarouselItems);
 			
 			for(var i = 0; i < completeCarouselItems; i++) {
 				
-				console.log("B apending form " + (i * carouselItemSize) + " to " + ((i * carouselItemSize) + carouselItemSize) +  " of " + item.items[0].images.length );
+				console.log("B apending form " + (i * carouselItemSize) + " to " + ((i * carouselItemSize) + carouselItemSize) +  " of " + availableImages );
 				
 				body.append($.carouselItem(
 						i * carouselItemSize, 
@@ -55,46 +64,61 @@ $(function(){
 			
 			if(rest != 0) {
 			
-				console.log("C apending form " +(item.items[0].images.length - rest) + " to " + item.items[0].images.length + " of " +item.items[0].images.length );
+				console.log("C apending form " +(item.items[0].images.length - rest) + " to " + availableImages + " of " + availableImages );
 				
 				body.append($.carouselItem(
 						item.items[0].images.length - rest, 
 						item.items[0].images.length, 
 						item.items[0].images, modalSelector));
 			}
+			
+//			var left = $("<a />", {
+//				class:"left carousel-control",
+//				"data-slide":"prev",
+//				href: "#carousel-"+item.id})
+//				.append($("<span />", {class:"glyphicon glyphicon-chevron-left"}));
+//			
+//			var right = $("<a />", {
+//				class:"right carousel-control",
+//				"data-slide":"next",
+//				href: "#carousel-"+item.id})
+//				.append($("<span />", {class:"glyphicon glyphicon-chevron-right"}));
+			
+			root.append(body);
+//			root.append(left);
+//			root.append(right);
+//		}
+		
+		
+		
+		if(needCarousel) {
+			var left = $("<a />", {
+				class:"left carousel-control",
+				"data-slide":"prev",
+				href: "#carousel-"+item.id})
+				.append($("<span />", {class:"glyphicon glyphicon-chevron-left"}));
+			
+			var right = $("<a />", {
+				class:"right carousel-control",
+				"data-slide":"next",
+				href: "#carousel-"+item.id})
+				.append($("<span />", {class:"glyphicon glyphicon-chevron-right"}));
+			
+			root.append(left);
+			root.append(right);
 		}
 		
-		$(modalSelector + " .modal-body-up img")
-		.on("mouseenter", function(e){
+		$(modalSelector + " .modal-body-up").append(root);
+		
+		$(modalSelector + " .modal-body-up .item")
+		.on("mouseenter", "img", function(e){
+			console.log(e)
 			$(modalSelector + " .modal-body-down img").attr("src", e.target.src);
 			// reset all
 			$(modalSelector + " .modal-body-up img").css("border", "1px solid #ddd");
 			// edit selected
-			$(e.target).css("border", "5px solid #999");
+			$(e.target).css("border", "1px solid #999");
 		});
-		
-		root.append(body);
-		var left = $("<a />", {
-			class:"left carousel-control",
-			"data-slide":"prev",
-			href: "#carousel-"+item.id})
-			.append($("<span />", {class:"glyphicon glyphicon-chevron-left"}));
-		
-		var right = $("<a />", {
-			class:"right carousel-control",
-			"data-slide":"next",
-			href: "#carousel-"+item.id})
-			.append($("<span />", {class:"glyphicon glyphicon-chevron-right"}));
-		
-		root.append(left);
-		root.append(right);
-		$(modalSelector + " .modal-body-up").append(root);
-//		$(".container").append(root);
-//		$("#carousel-"+item.id).carousel(
-//				{
-//			"interval": false
-//		}
-//				);
 	};
 	
 });
